4b71f17c891631331517e80fbcb378215a99b547
Rohit Kumar <rohitK79739@gmail.com>
2024-03-08 00:07:38 +0530

    layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
-
+import { Icons } from './Icon'
 const Navbar = () => {
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -12,6 +12,7 @@ const Navbar = () => {
 
                         <div className="ml-4 flex lg:ml-0">
                             <Link href='/'>
+                                <Icons.logo className="h-10 w-10"/>
                                 
                             </Link>
                         </div>
