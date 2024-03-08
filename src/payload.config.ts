d7994e3f658e1cffa24203ef94b9c21ebc1ecb2f
Rohit Kumar <rohitK79739@gmail.com>
2024-03-09 00:17:20 +0530

    ...

---
+++ src/payload.config.ts
@@ -0,0 +1,23 @@
+import {buildConfig} from 'payload/config'
+import {mongooseAdapter} from '@payloadcms/db-mongodb'
+import {slateEditor} from "@payloadcms/richtext-slate"
+import {webpackBundler} from "@payloadcms/bundler-webpack"
+export default buildConfig({
+    serverURL:process.env.NEXT_PUBLIC_SERVER_URL||' ',
+    collections:[],
+    routes:{
+        admin:'/sell'
+    },
+    admin:{
+        bundler:webpackBundler(),
+        meta: {
+            titleSuffix:'- DigitalHippo',
+            favicon: '/favicons.ico',
+            ogImage:'/thumbnail.jpg'
+        }
+    },
+    editor:slateEditor({}),
+    db:mongooseAdapter({
+        url:process.env.MONGODB_URL!,
+    }),
+})
